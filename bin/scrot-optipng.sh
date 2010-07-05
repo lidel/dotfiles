e1ddbcf893a4ee60c495a68580c8e4ed79839ef5
Marcin Rataj <m@lidel.org>
2010-07-05 22:53:28 +0200

    bin/scrot-optipng.sh

---
+++ bin/scrot-optipng.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# simple scrot wrapper that run
+# optipng (http://optipng.sourceforge.net/) on shots
+# (put here since dmenu does not know about .zshrc aliases)
+exec /usr/bin/scrot -e 'nice optipng -o7 -i1 $f ' $*
