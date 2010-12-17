8c3aef6a02dff38000a45b3e422813181b88c6b5
Marcin Rataj <m@lidel.org>
2010-12-18 00:15:33 +0100

    vaio-s2ram.sh: local pm-suspend wrapper

---
+++ bin/vaio-s2ram.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+XMONAD_DBUS_SESSION_BUS_ADDRESS=`cat /proc/$(pgrep xmonad)/environ | xargs -0 -n1 | G ^DBUS_SESSION_BUS | xargs`
+DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.sleep &>/dev/null
+sudo pm-suspend --quirk-vbe-post
+vaio-backlight-raw.sh 7
+#DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.wake &>/dev/null
+
