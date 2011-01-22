8f136b5d1e5dcf833b8917ad037b103f6a7a4f6f
Marcin Rataj <m@lidel.org>
2011-01-23 00:09:39 +0100

    xmonad: goldenRatio

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -77,12 +77,12 @@ lidelDarkTheme = defaultTheme { inactiveBorderColor = myInactiveBorderColor
 myLayout = maximize (tiled ||| tiled3 ||| tabbedBottom shrinkText lidelDarkTheme )
   where
      -- default tiling algorithm partitions the screen into two panes
-     tiled   = ResizableTall nmaster delta ratio []
+     tiled   = ResizableTall nmaster delta goldenRatio []
      tiled3  = ThreeCol nmaster delta ratio3
      -- The default number of windsws in the master pane
      nmaster = 1
      -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+     goldenRatio = toRational $ 2/(1 + sqrt 5 :: Double)
      ratio3  = -1/3
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
