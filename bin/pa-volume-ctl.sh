af1e0a607e30ffb9632f457ba231704acbbf6722
Marcin Rataj <m@lidel.org>
2011-02-10 19:07:32 +0100

    new PulseAudio volume control script

--- bin/pa-volume-ctl.sh
+++ bin/pa-volume-ctl.sh
@@ -1,32 +1,50 @@
-#!/bin/sh
-# (Simplified) PulseAudio Volume Control Script
-#   2010-07-05 - Marcin Rataj (http://github.com/lidel/dotfiles)
-#                * removed unused functions and added .vol pipe
-#   2010-05-20 - Gary Hetzel  (http://gzn.pastebin.com/F1tM6R2J)
+#!/bin/bash
+# PulseAudio volume control script
+# by Marcin Rataj (http://lidel.org)
+# Updates: http://github.com/lidel/dotfiles/
+# License: public domain
 
-SINK=0
-STEP=5
-MAXVOL=65537 # let's just assume this is the same all over
-#MAXVOL=`pacmd list-sinks | grep "volume steps" | cut -d: -f2 | tr -d "[:space:]"`
+# Features:
+#   - [up|down|mute] controls
+#   - digital amplification
+#   - default sink detection
 
-VOLPERC=`pacmd list-sinks | grep "volume" | head -n1 | cut -d: -f3 | cut -d% -f1 | tr -d "[:space:]"`
+FULL=$((0x10000)) # 100%
+STEP=$((0x0028F)) #  ~1%
 
-# alternative implementation
-# pacmd set-sink-volume 0 $(printf '0x%x' $(( $(pacmd dump|grep set-sink-volume|cut -f3 -d' '|head -1) + 0xf00)) )"
+DUMP=`pacmd dump`
+DEFAULT_SINK=`echo "$DUMP" | grep set-default-sink | cut -d " " -f 2`
+OLD_VOL=`echo "$DUMP" | grep "set-sink-volume $DEFAULT_SINK" | cut -d " " -f 3`
 
 case $1 in
     up)
-        VOLSTEP="$(( $VOLPERC+$STEP ))";;
+        NEW_VOL=$((OLD_VOL + STEP))
+        # no roof limit (digital amplification feature)
+        ;;
     down)
-        VOLSTEP="$(( $VOLPERC-$STEP ))";;
+        NEW_VOL=$((OLD_VOL - STEP))
+        # respect floor limit
+        if [ $NEW_VOL -lt $((0x00000)) ]
+        then
+            NEW_VOL=$((0x00000))
+        fi
+        ;;
+    mute)
+        MUTE=`echo "$DUMP" | grep "set-sink-mute $DEFAULT_SINK" | cut -d " " -f 3`
+        if [ $MUTE == "no" ]
+        then
+            pactl set-sink-mute $DEFAULT_SINK yes
+        else
+            pactl set-sink-mute $DEFAULT_SINK no
+        fi
+        exit $?;;
     *)
         echo "Usage: `basename $0` [up|down]"
         exit 1;;
 esac
 
-VOLUME="$(( ($MAXVOL/100) * $VOLSTEP ))"
-pacmd set-sink-volume $SINK $VOLUME > /dev/null
+pactl set-sink-volume $DEFAULT_SINK `printf "0x%X" $NEW_VOL`
 
 # pipe used by xmobar's PipeReader widget
-echo $VOLSTEP > ~/.vol
+echo $NEW_VOL*100/$FULL|bc > ~/.vol
 
