fbfac0e643df5ac3a5b669be66550bc7f2c38bb1
Marcin Rataj <m@lidel.org>
2011-03-13 13:07:52 +0100

    vaio-s2ram.sh: dbus fix

--- bin/vaio-s2ram.sh
+++ bin/vaio-s2ram.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-XMONAD_DBUS_SESSION_BUS_ADDRESS=`cat /proc/$(pgrep xmonad)/environ | xargs -0 -n1 | G ^DBUS_SESSION_BUS | xargs`
-DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.sleep &>/dev/null
+XMONAD_DBUS_SESSION_BUS_ADDRESS=`cat /proc/$(pgrep xmonad)/environ | xargs -0 -n1 | grep ^DBUS_SESSION_BUS | xargs`
+sudo -u lidel DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.sleep &>/dev/null
 sudo pm-suspend --quirk-vbe-post
 vaio-backlight-raw.sh 7
 #DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.wake &>/dev/null
