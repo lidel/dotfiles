95de70285dc93b1f83985578c524c7d2ad4393f2
Marcin Rataj <m@lidel.org>
2011-10-12 19:31:25 +0200

    scrot-optipng.sh: new path

--- bin/scrot-optipng.sh
+++ bin/scrot-optipng.sh
@@ -2,4 +2,4 @@
 # simple scrot wrapper that runs
 # optipng (http://optipng.sourceforge.net/) on shots
 # (put here since dmenu does not know about .zshrc aliases)
-exec /usr/bin/scrot -e 'mv $f ~/gfx/screenshots/ && nice optipng -o7 -i1 ~/gfx/screenshots/$f ' $*
+exec /usr/bin/scrot -e 'mkdir -p ~/TODO/screnshots && mv $f ~/TODO/screnshots && nice optipng -o7 -i0 ~/TODO/screnshots/$f' $*
