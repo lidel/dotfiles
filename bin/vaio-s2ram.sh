2d351cf45a2e6ebea1c74242df47043a48abceed
Marcin Rataj <m@lidel.org>
2012-02-26 12:06:53 +0100

    vaio-s2ram.sh: spring ;-)

--- bin/vaio-s2ram.sh
+++ bin/vaio-s2ram.sh
@@ -3,6 +3,6 @@
 #XMONAD_DBUS_SESSION_BUS_ADDRESS=`cat /proc/$(pgrep xmonad)/environ | xargs -0 -n1 | grep ^DBUS_SESSION_BUS | xargs`
 #sudo -u lidel DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.sleep &>/dev/null
 sudo pm-suspend #--quirk-vbe-post
-sudo /home/lidel/bin/vaio-fw21e-backlight2.sh 7
+sudo /home/lidel/bin/vaio-fw21e-backlight2.sh 8
 #DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.wake &>/dev/null
 
