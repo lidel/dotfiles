255b7336923fa01166eee77a2d6fd452e933e53d
Marcin Rataj <m@lidel.org>
2012-06-28 21:20:41 +0200

    vaio-s2ram.sh: regression fix

--- bin/vaio-s2ram.sh
+++ bin/vaio-s2ram.sh
@@ -2,7 +2,7 @@
 # legacy: no longer using psi, left as a reference
 #XMONAD_DBUS_SESSION_BUS_ADDRESS=`cat /proc/$(pgrep xmonad)/environ | xargs -0 -n1 | grep ^DBUS_SESSION_BUS | xargs`
 #sudo -u lidel DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.sleep &>/dev/null
-sudo pm-suspend #--quirk-vbe-post
-sudo /home/lidel/bin/vaio-fw21e-backlight2.sh 8
+sudo pm-suspend --quirk-vbe-post
+sudo /home/lidel/bin/vaio-fw21e-backlight2.sh 7
 #DBUS_SESSION_BUS_ADDRESS=$XMONAD_DBUS_SESSION_BUS_ADDRESS dbus-send --session --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.wake &>/dev/null
 
