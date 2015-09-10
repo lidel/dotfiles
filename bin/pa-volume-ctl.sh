cf1827da12ea37fcde4bf0dd35b7ea77ecc1bb97
Marcin Rataj <lidel@lidel.org>
2015-09-10 22:28:24 +0200

    Fix volume control with pactl from Pulseaudio 6.0
    More info at https://github.com/graysky2/pulseaudio-ctl/issues/25

--- bin/pa-volume-ctl.sh
+++ bin/pa-volume-ctl.sh
@@ -6,13 +6,13 @@ DEFAULT_SINK=`pacmd dump | grep set-default-sink | cut -d " " -f 2`
 
 case $1 in
     up)
-        exec pactl set-sink-volume $DEFAULT_SINK -- +1%
+        exec pactl set-sink-volume $DEFAULT_SINK +2%
         ;;
     down)
-        exec pactl set-sink-volume $DEFAULT_SINK -- -1%
+        exec pactl set-sink-volume $DEFAULT_SINK -2%
         ;;
     mute)
-        exec pactl set-sink-mute   $DEFAULT_SINK -- toggle
+        exec pactl set-sink-mute   $DEFAULT_SINK toggle
         ;;
     *)
         echo "Usage: `basename $0` [up|down|mute]"
