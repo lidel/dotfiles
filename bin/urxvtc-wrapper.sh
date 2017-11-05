9e9905982a53372925e9ebb0fcdfedd7c756ad24
Marcin Rataj <lidel@lidel.org>
2017-11-05 13:20:55 +0100

    urxvt: robust urxvtd detection

--- bin/urxvtc-wrapper.sh
+++ bin/urxvtc-wrapper.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 # This wrapper makes sure that all urxvtc instances
 # are handled by a single, lazy-loaded urxvtd daemon
+# default to ~
 cd $HOME
-[[ -n `pidof urxvtd` ]] && exec urxvtc "$@"
-if [[ -z `pidof urxvtd` ]]; then
-    urxvtd -q -o -f
-    exec urxvtc "$@"
+
+# If everything went well, urxvtc returns with an exit status of 0.
+# If contacting the daemon fails, it exits with the exit status 2.
+# In all other error cases it returns with status 1.
+urxvtc "$@"
+if [ $? -eq 2 ]; then
+	urxvtd -q -o -f
+	urxvtc "$@"
 fi
