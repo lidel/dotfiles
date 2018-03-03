8342d6db042fcfad1f62b68dda85a8276faf57be
Marcin Rataj <lidel@lidel.org>
2018-03-03 21:40:44 +0100

    pa-volume-ctl.sh: display current volume via OSD

--- bin/pa-volume-ctl.sh
+++ bin/pa-volume-ctl.sh
@@ -2,19 +2,32 @@
 # PulseAudio [up|down|mute] controls for default sink
 # Updates: http://github.com/lidel/dotfiles/
 
-DEFAULT_SINK=`pacmd dump | grep set-default-sink | cut -d " " -f 2`
+getdefaultsinkname() {
+    pacmd stat |
+    awk -F": " '/^Default sink name: /{print $2}'
+}
+
+getdefaultsinkvol() {
+    pacmd list-sinks |
+    grep -A 10 "name: <$(getdefaultsinkname)>" |
+    grep volume: |
+    grep -m1 -o -E '[[:digit:]]+%' |
+    sed 's/%//' --
+}
 
 case $1 in
     up)
-        exec pactl set-sink-volume $DEFAULT_SINK +2%
+        pactl set-sink-volume $(getdefaultsinkname) +2%
         ;;
     down)
-        exec pactl set-sink-volume $DEFAULT_SINK -2%
+        pactl set-sink-volume $(getdefaultsinkname) -2%
         ;;
     mute)
-        exec pactl set-sink-mute   $DEFAULT_SINK toggle
+        pactl set-sink-mute   $(getdefaultsinkname) toggle
         ;;
     *)
         echo "Usage: `basename $0` [up|down|mute]"
         exit 1;;
 esac
+
+notify-send 'Volume' -t 200 -i audio-volume-medium-symbolic -h int:value:$(getdefaultsinkvol)
